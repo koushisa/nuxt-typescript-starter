78fb6019a14033a2815d607d2049c833cb274092
Shinya Komatsu <31304738+koushisa@users.noreply.github.com>
2019-11-15 09:19:18 +0900

    ini

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-layout justify-center align-center>
+    Initial
+  </v-layout>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class extends Vue {}
+</script>
